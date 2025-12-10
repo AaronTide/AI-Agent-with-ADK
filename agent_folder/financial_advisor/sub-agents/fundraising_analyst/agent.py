9360d9524d7aa1a5cffc2baffbd69a26a28ca6d1
Arifa Rahman Sikder <165793165+AaronTide@users.noreply.github.com>
2025-12-10 21:04:57 +0600

    Create agent.py

---
+++ agent_folder/financial_advisor/sub-agents/fundraising_analyst/agent.py
@@ -0,0 +1,16 @@
+"""Fundrasing Analyst Agent for providing the strategies for startup fundraising """
+
+from google.adk import Agent
+from google.adk.tools import google_search
+
+from . import prompt
+
+MODEL="gemini-2.5-pro"
+
+fundraising_analyst_agent = Agent(
+    model=MODEL,
+    name="fundraising_analyst_agent",
+    instruction=prompt.FUNDRAISING_ANALYST_PROMPT,
+    output_key="fundraising_assessment_output",
+    tools=[google_search],
+)
