f54bd280621a9d0dbf312fd4ecafc6e3a7661a41
Arifa Rahman Sikder <165793165+AaronTide@users.noreply.github.com>
2025-12-11 18:38:46 +0600

    Update agent.py

--- agent_folder/financial_advisor/agent.py
+++ agent_folder/financial_advisor/agent.py
@@ -4,13 +4,11 @@ from google.adk.agents import LlmAgent
 from google.adk.tools.agent_tool import AgentTool
 
 from . import prompt
-from .sub_agents.data_analyst import data_analyst_agent
+from .sub_agents.market_sizing import market_sizing_agent
 from .sub_agents.fundraising_analyst import fundraising_analyst_agent
 
 from .sub_agents.grant_funding import grant_funding_agent
 
-#from .sub_agents.risk_analyst import risk_analyst_agent
-#from .sub_agents.trading_analyst import trading_analyst_agent
 
 
 MODEL = "gemini-2.5-pro"
@@ -23,7 +21,7 @@ financial_coordinator = LlmAgent(
         "guide startup founders through a structured process to receive financial "
         "advice by orchestrating a series of expert subagents. "
         " Get relevant information about their startup and help them "
-        "analyze a market ticker for their startup/ provided company"
+        "analyze the market size and TAM/SAM/SOM of startup "
          "and develop fundraising strategies for the startup"
          "and also find relevant grants and funding opportunities according to startup profile"
         
@@ -31,14 +29,13 @@ financial_coordinator = LlmAgent(
     instruction=prompt.FINANCIAL_COORDINATOR_PROMPT,
     output_key="financial_coordinator_output",
     tools=[
-        AgentTool(agent=data_analyst_agent),
+        AgentTool(agent=market_sizing_agent),
         AgentTool(agent=fundraising_analyst_agent),
         AgentTool(agent=grant_funding_agent),
 
-        #AgentTool(agent=grant_funding_agent),
-        #AgentTool(agent=execution_analyst_agent),
-        #AgentTool(agent=risk_analyst_agent),
+        
     ],
+    
 )
 
 root_agent = financial_coordinator
